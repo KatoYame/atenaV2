d90d0d2d40f1d2f6278e8876f1e471b7a4dbc263
KatoYame <94295237+KatoYame@users.noreply.github.com>
2021-11-16 11:53:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Source Code from NekoBotz (DanishGanz)
 
 global.linkGC = ['https://chat.whatsapp.com/INua3dEj25B0eFrHma0OsC', 'https://chat.whatsapp.com/B9bGT4rmw8PFnTYCYRcNab', 'https://chat.whatsapp.com/B9bGT4rmw8PFnTYCYRcNab', 'https://chat.whatsapp.com/DrJE1mDfhyj3Q4xHp9HeL4'] // ganti jadi group lu
-global.owner = ['6282317022706', '62822481929171', '6281212924676', '6282317022706'] // Put your number here
+global.owner = ['6282317022706', '6282248192917', '6281212924676', '6282317022706'] // Put your number here
 global.mods = ['6285904398191'] // Want some help?
 global.prems = ['6283819142257', '6285695484734', '6285604708139', '6282317022706'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
